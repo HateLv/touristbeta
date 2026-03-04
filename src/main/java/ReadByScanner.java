2e28cafd8d5e4458a2d641e7f63ae66908aceb5a
HateLv <erikulme1213@mail.com>
2026-03-04 15:26:45 +0100

    third

---
+++ src/main/java/ReadByScanner.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadByScanner {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        String path = "src/main/resources/name.txt";
+        File file = new File(path);
+
+        Scanner sc = new Scanner(file);
+
+        while(sc.hasNext()){
+            System.out.println(sc.nextLine());
+        }
+
+    }
+}
+
